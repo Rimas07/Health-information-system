cabb60202188e52a99aaf78ed2c3c2686be9f6f9
Rimas07 <53482924+Rimas07@users.noreply.github.com>
2026-04-30 21:51:55 +0500

    remove old frontend.html handler, serve-static handles root

--- His/src/app.controller.ts
+++ His/src/app.controller.ts
@@ -1,14 +1,7 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import type { Response } from 'express';
-import { join } from 'path';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
-
-  @Get()
-  serveFrontend(@Res() res: Response) {
-    res.sendFile(join(__dirname, '..', 'frontend.html'));
-  }
+  constructor(private readonly appService: AppService) {}
 }
